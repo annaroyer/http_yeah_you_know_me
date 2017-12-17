97caf4441aa8e0fa1d6da93cc8a40fe3835bbf2f
Anna Royer <anna.royer@colorado.edu>
2017-12-17 09:46:12 -0700

    add faraday tests

--- test/server_test.rb
+++ test/server_test.rb
@@ -1,8 +1,11 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'Faraday'
 require './lib/http'
 
 class ServerTest < Minitest::Test
-
+  def test_request_takes_requests_to_server_and_outputs_responses
+    
+  end
 end
