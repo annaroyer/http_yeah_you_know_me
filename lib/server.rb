d455776ef5092fd50ce2ec3ff55712ebb87e4f5a
Anna Royer <anna.royer@colorado.edu>
2017-12-17 20:14:31 -0700

    add it1-3 functionality to master

--- lib/server.rb
+++ lib/server.rb
@@ -1,55 +1,39 @@
 require 'socket'
+require './lib/request'
 require 'pry'
 
 class Server
-  PORT = 9292
   def initialize
-    @server = TCPServer.new(PORT)
-    @requests = 0
-    @path = nil
+    @server = TCPServer.new(9292)
+    @request_counter = 0
   end
 
   def request
-    until @path == '/shutdown' do
+    loop do
       client = @server.accept
-      format_request(client.readpartial(2048))
-      @requests += 1
+      @request = Request.new(client.readpartial(2048))
+      @request_counter += 1
       client.puts headers + output
       client.close
     end
   end
 
-  def format_request(client_request)
-    verb, @path, protocol = client_request.lines[0].split(' ')
-    @formatted_request = "Verb: #{verb}\nPath: #{@path}\nProtocol: #{protocol}\n"
-    host = client_request.lines[1][/:.*:/].gsub(':', '')
-    @formatted_request += "Host:#{host}\nPort: #{PORT}\nOrigin:#{host}\n"
-    @formatted_request += client_request.lines[6]
-  end
-
   def route
-    if @path == '/'
-      "#{@formatted_request}"
-    elsif @path == '/hello'
-      "Hello World(#{@requests})"
-    elsif @path == '/datetime'
-      "#{Time.now.strftime("%I:%M%p on %A, %B %-d, %Y")}"
-    elsif @path == '/shutdown'
-      "Total requests: #{@requests}"
-    elsif @path == '/word_search'
-      search_word(param)
+    response = case @request.path
+      when '/hello' then "Hello World! (#{@request_counter})"
+      when '/datetime' then Time.now.strftime("%I:%M%p on %A, %B %-d, %Y")
+      when '/shutdown' then "Total requests: #{@request_counter}"
+      when '/word_search' then search_word(@request.param)
     end
+    return response
   end
 
-  def search_word(param)
-    dictionary = File.open('/usr/share/dict/words', 'r')
-    if dictionary.include?(param)
-      return "#{param} is a known word"
-    else
-      return "#{param} is not a known word"
+  def search_word(word)
+    File.readlines('/usr/share/dict/words').each do |line|
+      return "#{word} is a known word" if word == line.chomp
     end
+    "#{word} is not a known word"
   end
-
 
   def headers
     ["http/1.1 200 ok",
@@ -59,12 +43,11 @@ class Server
      "content-length: #{output.length}\r\n\r\n"].join("\r\n")
   end
 
-  def response
-    "<pre>\n#{route}</pre>"
-  end
-
   def output
-    "<html><head></head><body>#{response}</body></html>"
+    "<html><head></head><body><pre>
+    #{route}
+    #{@request.format}
+    </pre></body></html>"
   end
 end
 
