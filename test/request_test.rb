46f020e7467dddada29a42db5b34876aedc393c3
Anna Royer <anna.royer@colorado.edu>
2017-12-18 14:33:50 -0700

    add and pass all request parser class tests

--- test/request_test.rb
+++ test/request_test.rb
@@ -1,8 +1,9 @@
 require './test/test_helper'
+require './lib/request'
 
 class RequestTest < Minitest::Test
   def setup
-    @request_string = "GET / HTTP/1.1
+    @request_string = "GET /word_search?word=hello HTTP/1.1
     Host: 127.0.0.1:9292
     Connection: keep-alive
     Cache-Control: max-age=0
@@ -11,7 +12,7 @@ class RequestTest < Minitest::Test
     User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36
     Accept-Encoding: gzip, deflate, sdch
     Accept-Language: en-US,en;q=0.8"
-end
+  end
 
   def test_it_takes_a_request_string_and_finds_the_verb
     request = Request.new(@request_string)
@@ -22,7 +23,7 @@ end
   def test_it_takes_a_request_string_and_finds_the_path
     request = Request.new(@request_string)
 
-    assert_equal "/", request.path
+    assert_equal "/word_search", request.path
   end
 
   def test_it_takes_a_request_string_and_finds_the_verb
@@ -31,7 +32,22 @@ end
     assert_equal "GET", request.verb
   end
 
+  def test_it_takes_a_request_string_and_finds_the_path_param
+    request = Request.new(@request_string)
 
+    assert_equal "hello", request.param
+  end
 
+  def test_it_takes_a_request_string_and_reformats_the_important_values
+    request = Request.new(@request_string)
 
+    formatted = "
+    Verb: GET
+    Path: /word_wearch
+    Protocol: http:/1.1
+    Host: 127.0.0.1
+    Port: 9292
+    Origin: 127.0.0.1
+    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+  end
 end
