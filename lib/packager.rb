4233ef5a179f2a6c1071bbd0797793e019ac872f
Anna Royer <anna.royer@colorado.edu>
2017-12-21 17:27:02 -0700

    add packager method

--- lib/packager.rb
+++ lib/packager.rb
@@ -1,3 +1,12 @@
 class Packager
-  
+  def initialize(path)
+    @path = path
+  end
+
+  def wrap_html(response)
+    "<html><head></head><body><pre>
+    #{response}
+    </pre></body></html>"
+  end
+
 end
