b0b88412bbdc9f455269e158be2a0beed0bf61ef
Anna Royer <anna.royer@colorado.edu>
2017-12-21 23:05:18 -0700

    finish flexible headers

--- lib/request.rb
+++ lib/request.rb
@@ -29,7 +29,7 @@ class Request
   end
 
   def location
-    return "Location: http://#{@host}:#{@port}#{@path}"
+    return "Location: http://#{@host}:#{@port}#{@path}\r\n"
   end
 
   def format
--- lib/wrapper.rb
+++ lib/wrapper.rb
@@ -11,31 +11,16 @@ class Wrapper
 
   def output
     if @request.verb == 'POST'
-      return flexible_headers
+      @location = @request.location
+      return headers
     else
-      return basic_headers + @output
+      return headers + @output
     end
   end
 
-  def basic_headers
-    "http/1.1 200 ok
-     date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}
-     server: ruby
-     content-type: text/html; charset=iso-8859-1
-     content-length: #{@output.length}\r\n\r\n"
-  end
-
-  def redirect_headers
-    ["HTTP/1.1 302 Moved Permanently",
-     "Location: http://127.0.0.1:9292/start_game",
-     "Server: ruby",
-     "Content-Type: text/html; charset=iso-8859-1",
-     "Content-Length: #{@output.length}\r\n\r\n"].join("\r\n")
-  end
-
-  def flexible_headers
+  def headers
     ["HTTP/1.1 302 Moved Permanently\r\n",
-     "#{@request.location}\r\n",
+     @location,
      "Server: ruby\r\n",
      "Content-Type: text/html; charset=iso-8859-1\r\n",
      "Content-Length: #{@output.length}\r\n\r\n"].join
